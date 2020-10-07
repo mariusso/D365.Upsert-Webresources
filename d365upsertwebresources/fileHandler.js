ac3208962e378dc3a0ce88b5e0b1ca454f025be1
Marius Solend <marius.solend@acando.no>
2020-10-07 14:03:25 +0200

    Minor changes

--- d365upsertwebresources/fileHandler.js
+++ d365upsertwebresources/fileHandler.js
@@ -21,25 +21,25 @@ const ReadDirectoryAndRetrieveFiles = (directory, virtualPath, existingFileList 
 
     const fileList = existingFileList || [];
 
-    for(const name of namesInDirectory) {
+    for (const name of namesInDirectory) {
 
         const completePath = path.resolve(directory, name);
         const stats = fs.statSync(completePath);
         const isDirectory = stats.isDirectory();
 
-        if(!isDirectory) {
-            
-            if(fileNames.length > 0 && !fileNames.some(f => f === name)) {
+        if (!isDirectory) {
+
+            if (fileNames.length > 0 && !fileNames.some(f => f === name)) {
                 continue;
             }
 
             const fileContent = fs.readFileSync(completePath, { encoding: "base64" });
-                fileList.push({
-                    fileName: name,
-                    virtualPath: path.join(virtualPath, name).replace("\\", "/"),
-                    fileContent: fileContent,
-                    type: GetWebResourceType(name),
-                });
+            fileList.push({
+                fileName: name,
+                virtualPath: path.join(virtualPath, name).replace(/[\\]/g, "/", ),
+                fileContent: fileContent,
+                type: GetWebResourceType(name),
+            });
 
         } else {
             ReadDirectoryAndRetrieveFiles(completePath, path.join(virtualPath, name), fileList, fileNames);
@@ -52,43 +52,43 @@ const ReadDirectoryAndRetrieveFiles = (directory, virtualPath, existingFileList 
 const GetWebResourceType = (fileName) => {
     const extension = path.extname(fileName);
 
-    if(extension.toLowerCase() == ".htm" || extension.toLowerCase() == ".html") {
+    if (extension.toLowerCase() == ".htm" || extension.toLowerCase() == ".html") {
         return 1;
     }
 
-    if(extension.toLowerCase() == ".css") {
+    if (extension.toLowerCase() == ".css") {
         return 2;
     }
 
-    if(extension.toLowerCase() == ".js") {
+    if (extension.toLowerCase() == ".js") {
         return 3;
     }
 
-    if(extension.toLowerCase() == ".xml") {
+    if (extension.toLowerCase() == ".xml") {
         return 4;
     }
 
-    if(extension.toLowerCase() == ".png") {
+    if (extension.toLowerCase() == ".png") {
         return 5;
     }
 
-    if(extension.toLowerCase() == ".jpg") {
+    if (extension.toLowerCase() == ".jpg") {
         return 6;
     }
 
-    if(extension.toLowerCase() == ".gif") {
+    if (extension.toLowerCase() == ".gif") {
         return 7;
     }
 
-    if(extension.toLowerCase() == ".xap") {
+    if (extension.toLowerCase() == ".xap") {
         return 8;
     }
 
-    if(extension.toLowerCase() == ".xsl" || extension.toLowerCase() == ".xslt") {
+    if (extension.toLowerCase() == ".xsl" || extension.toLowerCase() == ".xslt") {
         return 9;
     }
 
-    if(extension.toLowerCase() == ".ico") {
+    if (extension.toLowerCase() == ".ico") {
         return 10;
     }
 }
